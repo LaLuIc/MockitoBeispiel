c8af8f8dfd4b67656026db9a9fc09d2474b23d4f
LaLuIc <lauraichters@web.de>
2015-05-18 16:39:35 +0200

    Initial Commit

---
+++ src/main/java/de/dhbw/se/de/dhbw/se/App.java
@@ -0,0 +1,13 @@
+package de.dhbw.se.de.dhbw.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
