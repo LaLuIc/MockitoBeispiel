3bc4ffaa13afc491e2ef0d39ea14e1dcbd723a9b
LaLuIc <lauraichters@web.de>
2015-05-19 13:59:07 +0200

    Fertiges Projekt

--- src/test/java/de/dhbw/se/ServiceTest.java
+++ src/test/java/de/dhbw/se/ServiceTest.java
@@ -12,27 +12,43 @@ import static org.mockito.Mockito.verify;
 
 public class ServiceTest {
 	
+	Service service;
 	
 	
 	@Before
 	public void setup(){
-		
+		Addierer addierer = mock(Addierer.class);
+		when(addierer.add(3, 3)).thenReturn(6);
+		service =new Service();
+		service.setAddierer(addierer);
 	}
 
 	@Test
 	public void testAddTwoNumbers(){
-		
+		Assert.assertEquals(6, service.addTwoNumbers(3,3));
 	}
 	
 
 	
 	@Test
 	public void testVerify()  {
+		Addierer addierer = mock(Addierer.class);
+		when(addierer.add(3, 3)).thenReturn(6);
+		service =new Service();
+		service.getAddierer();
 		
+		verify(addierer, times(1));
 	}
 	
 	@Test
 	public void testSpy() {
-					  
+		Addierer addierer = mock(Addierer.class);
+		when(addierer.add(3, 3)).thenReturn(6);
+		service =new Service();
+		service.setAddierer(addierer);
+		
+		Service serviceSpy = spy(service);
+		
+		Assert.assertEquals(6, serviceSpy.addTwoNumbers(3, 3));
 	}
 }
